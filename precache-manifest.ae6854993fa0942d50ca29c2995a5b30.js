4d314ac13b9ff2f39fc827b327ccacbbfbd3cc05
Kate <ket.mak.95@gmail.com>
2019-04-25 12:54:16 +0300

    Updates

---
+++ precache-manifest.ae6854993fa0942d50ca29c2995a5b30.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5daf1e6574934a7d0a9b",
+    "url": "/user-list/static/js/runtime~main.b6747a5c.js"
+  },
+  {
+    "revision": "83693eeb5467c33c5dca",
+    "url": "/user-list/static/js/main.5add8c0a.chunk.js"
+  },
+  {
+    "revision": "fa9eaee1f1b1ea0039bf",
+    "url": "/user-list/static/js/2.3e4d6022.chunk.js"
+  },
+  {
+    "revision": "83693eeb5467c33c5dca",
+    "url": "/user-list/static/css/main.4cdc2a8f.chunk.css"
+  },
+  {
+    "revision": "fa9eaee1f1b1ea0039bf",
+    "url": "/user-list/static/css/2.739994aa.chunk.css"
+  },
+  {
+    "revision": "86756a091bdb7d9e5d8db4fbc28e01c1",
+    "url": "/user-list/index.html"
+  }
+];
